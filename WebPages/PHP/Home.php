80acef7518d52fb57747171a154622fdd8f9e215
jeff1861 <36648387+jeff1861@users.noreply.github.com>
2018-03-25 21:10:31 -0400

    Added Files
    Added the Home Page file and the Home page CSS file

---
+++ WebPages/PHP/Home.php
@@ -0,0 +1,148 @@
+<!DOCTYPE HTML5>
+<html>
+<head>
+  <title>The ForEach Store</title>
+  <link rel="stylesheet" href="../CSS/Home.css">
+</head>
+<body>
+  <div class='welcomelable'>
+    <label>Welcome to the ForEach Store!!</label><br>
+  </div>
+  <label class='catagories'>Popular Games</label><br>
+  <div class="scrollmenu">
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+  </div><br><br>
+  <label class='catagories'>New Games</label><br>
+  <div class="scrollmenu">
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+  </div><br><br>
+  <label class='catagories'>Discounted Games</label><br>
+  <div class="scrollmenu">
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+  </div><br><br>
+  <label class='catagories'>Free Games</label><br>
+  <div class="scrollmenu">
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+    <a href="">Game</a>
+  </div>
+</body>
+</html>
