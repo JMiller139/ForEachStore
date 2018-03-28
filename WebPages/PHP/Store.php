260ea3970ce98e586fb57fedf48cb89298263d28
JMiller139 <jmiller517@radford.edu>
2018-03-28 18:39:06 -0400

    added filler
    added filler pictures to the store.php

--- WebPages/PHP/Store.php
+++ WebPages/PHP/Store.php
@@ -9,7 +9,7 @@
     <title>ForEach Store</title>
 </head>
 
-</body>
+<body>
   <!-- BANNER NEEDS TO BE INSERTED -->
 
   <table id='filterTable'>
@@ -37,26 +37,27 @@
     <!-- php should generate this table according to the amount of games
          returned from the query -->
     <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+      <td><img src="https://images-eds-ssl.xboxlive.com/image?url=8Oaj9Ryq1G1_p3lLnXlsaZgGzAie6Mnu24_PawYuDYIoH77pJ.X5Z.MqQPibUVTc1FhCVDaoswL6bmTkmdwXXVGD6yxKE8umhRvOrmVN0xbkzXrlP.4vwk9ff_1PFWsPIkRF021Is.9NoMYqy230P7wL6hYZStpGgfe.yhZLY8Y71cJgI6s._21pXd9N_mrL4lryj8rWOkO5tlBR2fUvtThsMXmdMQrp5WHy0AvaCaE-&w=800&h=800&format=jpg" alt="Fortnite" height="175px" width="225px"></td>
+      <td><img src="https://images-na.ssl-images-amazon.com/images/I/81LXUOTmcGL.jpg" alt="Rocket League" height="175px" width="225px"></td>
+      <td><img src="https://cdn.gamer-network.net/2017/articles/2017-10-23-15-36/GTA_5_Online_LEAK_Duke_O_Death_DLC_update_coming_soon_605675.jpg/EG11/resize/300x-1/quality/75/format/jpg" alt="GTA5" height="175" width="225>"</td>
+      <td><img src="https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Fallout_4_cover_art.jpg/220px-Fallout_4_cover_art.jpg" alt="Fallout4" height="175" width="225>"</td></td>
+      <td><img src="https://vignette.wikia.nocookie.net/lotr/images/2/2b/Middle-earth_Shadow_of_War.jpg/revision/latest?cb=20170302153716" alt="Shadow of War" height="175" width="225>"</td></td>
     </tr>
     <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+      <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdTaXVL-hk0eEDHXvfBJv2uqJ6msPE0irZqtA9i5OnNTQ-IclG" alt="Forza Horizon 3" height="175" width="225>"</td></td>
+      <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdVD6yBci8ko-txo0sFioPFeV9kie3LQPZo7frQO5MMX2I8vyI" alt="Borderlands 2" height="175" width="225>"</td></td>
+      <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvXoarPvljhJn28MKOTGkXo1_zt0-5AT0thG-SXGDqahjvAG2t8g" alt="Overwatch" height="175" width="225>"</td></td>
+      <td><img src="https://bioshockinfinite.ghoststorygames.com/site/uploads/sites/3/synopsis_graphic01-323x254.jpg" alt="Bioshock Infinite" height="175" width="225>"</td></td>
+      <td><img src="https://static.gamespot.com/uploads/scale_tiny/1197/11970954/3246063-aco_pack_agnostic_bxsht2d_e3_170611_330pm_us.jpg" alt="Assassins Creed Origins" height="175" width="225>"</td></td>
     </tr>
     <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+      <td><img src="" alt="" height="" width=""></td>
+      <td><img src="" alt="" height="" width=""></td>
+      <td><img src="" alt="" height="" width=""></td>
+      <td><img src="" alt="" height="" width=""></td>
+      <td><img src="" alt="" height="" width=""></td>
     </tr>
+    <!-- <img src="" alt="" height="" width=""> -->
     <tr>
       <td></td>
       <td></td>
