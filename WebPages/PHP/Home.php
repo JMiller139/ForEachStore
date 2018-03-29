d16476dd1215bd84596729bc15cb645c52a82637
Jeff <jreoch1@radford.edu>
2018-03-28 20:00:02 -0400

    Added Images to the home page

--- WebPages/PHP/Home.php
+++ WebPages/PHP/Home.php
@@ -9,140 +9,43 @@
     <label>Welcome to the ForEach Store!!</label><br>
   </div>
   <label class='catagories'>Popular Games</label><br>
+  <!-- need to be auto generated for each scroolmenu-->
   <div class="scrollmenu">
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
+    <img src="https://images-eds-ssl.xboxlive.com/image?url=8Oaj9Ryq1G1_p3lLnXlsaZgGzAie6Mnu24_PawYuDYIoH77pJ.X5Z.MqQPibUVTc1FhCVDaoswL6bmTkmdwXXVGD6yxKE8umhRvOrmVN0xbkzXrlP.4vwk9ff_1PFWsPIkRF021Is.9NoMYqy230P7wL6hYZStpGgfe.yhZLY8Y71cJgI6s._21pXd9N_mrL4lryj8rWOkO5tlBR2fUvtThsMXmdMQrp5WHy0AvaCaE-&w=800&h=800&format=jpg" alt="Fortnite" height="125px" width="175px">
+    <img src="https://images-na.ssl-images-amazon.com/images/I/81LXUOTmcGL.jpg" alt="Rocket League" height="125px" width="175px">
+    <img src="https://cdn.gamer-network.net/2017/articles/2017-10-23-15-36/GTA_5_Online_LEAK_Duke_O_Death_DLC_update_coming_soon_605675.jpg/EG11/resize/300x-1/quality/75/format/jpg" alt="GTA5" height="125" width="175">
+    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Fallout_4_cover_art.jpg/220px-Fallout_4_cover_art.jpg" alt="Fallout4" height="125" width="175">
+    <img src="https://vignette.wikia.nocookie.net/lotr/images/2/2b/Middle-earth_Shadow_of_War.jpg/revision/latest?cb=20170302153716" alt="Shadow of War" height="125" width="175">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdTaXVL-hk0eEDHXvfBJv2uqJ6msPE0irZqtA9i5OnNTQ-IclG" alt="Forza Horizon 3" height="125" width="175">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdVD6yBci8ko-txo0sFioPFeV9kie3LQPZo7frQO5MMX2I8vyI" alt="Borderlands 2" height="125" width="175">
   </div><br><br>
   <label class='catagories'>New Games</label><br>
   <div class="scrollmenu">
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
+    <img src="https://sslh.ulximg.com/image/740x493/cover/1522167121_baf1a248c1fd96bd266e2c743127f254.jpg/91d447a8443b7bf3022a15ec049ea9cd/1522167121_4e3b05014fe38ed4df4dacf393e16ef8.jpg" alt="Far Cry 5" height="125" width="175">
+    <img src="https://cdn3.dualshockers.com/wp-content/uploads/2018/01/MonsterHunterWorld.jpg" alt="monster hunter world" height="125px" width="175px">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9I6a5RAX61VKsyW11Sj7ycMan-8JwnIPkRyRI6JezNSyIU7XF" alt="dragon ball fighterz" height="125px" width="175px">
+    <img src="https://www.thenerdmag.com/wp-content/uploads/2017/04/CODWW2_1.png" alt="cod WW2" height="125px" width="175px">
+    <img src="https://img.youtube.com/vi/cwGrDlv5wok/hqdefault.jpg" alt="hellblade senua's sacrifice" height="125px" width="175px">
+    <img src="https://images.g2a.com/newlayout/600x351/1x1x0/336d3b5a7ec8/59e73f58ae653aa7b752ff84" alt="prey" height="125px" width="175px">
   </div><br><br>
   <label class='catagories'>Discounted Games</label><br>
   <div class="scrollmenu">
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
+    <img src="https://static.giantbomb.com/uploads/original/32/329701/2993791-survival-battle-royale-darwin-project-commences-pc-open-beta-with-mixer-features-1.jpg" alt="The Darwin Project" height="125" width="175">
+    <img src="http://i.playground.ru/i/95/18/22/00/blog/icon.400x225.jpg?v1" alt="Space Engineers" height="125" width="175">
+    <img src="https://static.quizur.com/i/b/5702fe733d3b39.92828718mortal-kombat-x-enhanced-online-beta-header.jpg" alt="mortal kombat x" height="125" width="175">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdVD6yBci8ko-txo0sFioPFeV9kie3LQPZo7frQO5MMX2I8vyI" alt="Borderlands 2" height="125" width="175">
+    <img src="https://cdn.gamer-network.net/2017/articles/2017-10-23-15-36/GTA_5_Online_LEAK_Duke_O_Death_DLC_update_coming_soon_605675.jpg/EG11/resize/300x-1/quality/75/format/jpg" alt="GTA5" height="125" width="175">
+    <img src="https://images-na.ssl-images-amazon.com/images/I/81LXUOTmcGL.jpg" alt="Rocket League" height="125px" width="175px">
   </div><br><br>
   <label class='catagories'>Free Games</label><br>
   <div class="scrollmenu">
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
-    <a href="">Game</a>
+    <img src="https://images-eds-ssl.xboxlive.com/image?url=8Oaj9Ryq1G1_p3lLnXlsaZgGzAie6Mnu24_PawYuDYIoH77pJ.X5Z.MqQPibUVTc1FhCVDaoswL6bmTkmdwXXVGD6yxKE8umhRvOrmVN0xbkzXrlP.4vwk9ff_1PFWsPIkRF021Is.9NoMYqy230P7wL6hYZStpGgfe.yhZLY8Y71cJgI6s._21pXd9N_mrL4lryj8rWOkO5tlBR2fUvtThsMXmdMQrp5WHy0AvaCaE-&w=800&h=800&format=jpg" alt="Fortnite" height="125px" width="175px">
+    <img src="http://cdn.player.one/sites/player.one/files/styles/lg/public/2018/02/13/eve-online-v-day.jpg" alt="eve online" height="125px" width="175px">
+    <img src="https://cdn.images.express.co.uk/img/dynamic/143/590x/Smite-patch-notes-819381.jpg" alt="smite" height="125px" width="175px">
+    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/5/5f/Tf2_standalonebox.jpg/220px-Tf2_standalonebox.jpg" alt="team fortress 2" height="125px" width="175px">
+    <img src="https://steamcdn-a.akamaihd.net/steam/apps/570/header.jpg?t=1520987245" alt="dota 2" height="125px" width="175px">
+    <img src="https://res.cloudinary.com/lmn/image/upload/c_limit,h_360,w_640/e_sharpen:100/f_auto,fl_lossy,q_auto/v1/gameskinnyc/g/a/m/gaming-20170224-paladins-91db3.jpg" alt="paladins" height="125px" width="175px">
+    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/b/bd/Warframe_Cover_Art.png/220px-Warframe_Cover_Art.png" alt="warframe" height="125px" width="175px">
   </div>
 </body>
 </html>
